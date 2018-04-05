54390b3f2ec41d7cfdcb2f3c44980580fdae6e02
* <hilariojunior2@gmail.com>
2018-04-05 05:29:45 -0500

    instalador

---
+++ smlinstaller.sh
@@ -0,0 +1,20 @@
+#!bash/bin
+#Script instalador de SMLoadr
+#Created for Juni0r2019
+echo "Actualizando Paquetes...\n"
+pkg update
+pkg upgrade
+echo "Instalanto Node.js y Git...\n"
+pkg install nodejs git
+echo "Clonando SMLoadr...\n"
+git clone https://git.teknik.io/SMLoadrDev/SMLoadr.git
+echo "Configurando instalacion...\n"
+mv SMLoadr/{package.json,SMLoadr.js} ./
+rm -Rf SMLoadr
+npm install
+sed -i s/linux/android/ node_modules/openurl/openurl.js
+mkdir ~/storage/shared/SMLoadr
+ln -s ~/storage/shared/SMLoadr DOWNLOADS
+echo "Para iniciar poner node SMLoadr.js en la terminal\n"
+echo "La capeta de musica estara en la memoria interna -> SMLoadr, separada por artista y album"
+
